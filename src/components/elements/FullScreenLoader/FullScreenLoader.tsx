e0641bcb96c81f2b5875a173becaedfd2f9cd22c
Tomas <tomas_forman@outlook.com>
2021-06-05 15:10:29 -0300

    fix loader scss import

--- src/components/elements/FullScreenLoader/FullScreenLoader.tsx
+++ src/components/elements/FullScreenLoader/FullScreenLoader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './FullscreenLoader.module.scss';
+import styles from './FullScreenLoader.module.scss';
 import {ProgressSpinner} from 'primereact/progressspinner';
 
 type Props = {
