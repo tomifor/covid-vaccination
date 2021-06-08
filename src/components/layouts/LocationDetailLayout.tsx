fed9e72a5913b344a91c0c5145df99589d41a9f1
Tomas <tomas_forman@outlook.com>
2021-06-07 23:12:57 -0300

    add compare button

--- src/components/layouts/LocationDetailLayout.tsx
+++ src/components/layouts/LocationDetailLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from '../../../styles/LocationDetail.module.scss';
 import {getIndicators} from '../../utils/parser';
 import {VaccinationDto} from '../../models/vaccination.model';
@@ -6,6 +6,7 @@ import {Option} from '../../models/options.model';
 import LocationIndicators from '../modules/LocationIndicators';
 import {getDailyAppliedDoses} from '../../utils/chartsUtils';
 import DailyAppliedDoses from '../modules/charts/DailyAppliedDoses';
+import Button from '../elements/Button/Button';
 
 type Props = {
   data: VaccinationDto;
@@ -14,6 +15,7 @@ type Props = {
 const LocationDetailLayout: React.FC<Props> = ({data, location}): JSX.Element => {
   const indicators = getIndicators(data);
   const dailyAppliedDosesData = getDailyAppliedDoses(data);
+  const [comparisonModalForm, setComparisonModalForm] = useState<boolean>(false);
 
   return (
     <div className={styles.container}>
@@ -21,10 +23,16 @@ const LocationDetailLayout: React.FC<Props> = ({data, location}): JSX.Element =>
         <h1 className={styles.title}>{location?.label}</h1>
         <p className={styles.population}>Población total: {indicators.totalPopulation}</p>
       </div>
-      <div className={'p-text-center'}>
-        <p className={'p-mb-1 p-mt-2'}>Última actualización: {indicators.lastUpdate}</p>
-      </div>
+
       <div className={styles.bodyContainer}>
+        <div className={styles.lastUpdate + ' p-d-flex p-jc-between p-ai-center p-mt-2'}>
+          <p className={'p-mb-1 p-mt-2'}>Última actualización: {indicators.lastUpdate}</p>
+          <Button
+            className={'p-button-outlined p-button-sm'}
+            label={'Comparar'}
+            onClick={() => setComparisonModalForm(true)}
+          />
+        </div>
         <LocationIndicators indicators={indicators} />
         <DailyAppliedDoses data={dailyAppliedDosesData} />
       </div>
