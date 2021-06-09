f662258dbd156c617827b65715dd3b33f5573077
Tomas <tomas_forman@outlook.com>
2021-06-09 18:07:23 -0300

    add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,6 +25,17 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Squada+One&display=swap"
             rel="stylesheet"
           />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-K0SH4JFB9Q" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-K0SH4JFB9Q');
+            `,
+            }}
+          />
         </Head>
         <body>
           <Main />
